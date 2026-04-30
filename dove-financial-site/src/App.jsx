4b1c474cdb3e0374c72c41a5960114002a2a7fa9
hiitlegends2021 <hiitlegends2021@gmail.com>
2026-04-30 01:26:33 -0500

    Restore full site with cash flow rescue page

--- dove-financial-site/src/App.jsx
+++ dove-financial-site/src/App.jsx
@@ -1,13 +1,56 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Services from "./Services";
+import Products from "./Products";
+import Framework from "./Framework";
+import Audience from "./Audience";
+import WhyDove from "./WhyDove";
+import Workshops from "./Workshops";
+import Testimonials from "./Testimonials";
+import CTASection from "./CTASection";
+import Footer from "./Footer";
+
+import ServicesPage from "./pages/ServicesPage";
+import ProductsPage from "./pages/ProductsPage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
 import CashFlowRescue from "./pages/CashFlowRescue";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Products />
+      <Framework />
+      <Audience />
+      <WhyDove />
+      <Workshops />
+      <Testimonials />
+      <CTASection />
+    </>
+  );
+}
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1 style={{ padding: 40 }}>Home Works</h1>} />
-      <Route path="/cash-flow-rescue" element={<CashFlowRescue />} />
-    </Routes>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/cash-flow-rescue" element={<CashFlowRescue />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
